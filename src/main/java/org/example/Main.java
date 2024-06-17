9582c98c3d35f116035047ac8ffbc2070fbd2795
Negus007 <141578316+Negus007@users.noreply.github.com>
2024-06-17 17:19:48 -0400

    added/created bot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+        JDA bot = JDABuilder.createDefault(System.getenv("BOT_KEY"))
+                .enableIntents(GatewayIntent.GUILD_PRESENCES)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .setActivity(Activity.playing("Gloomhaven"))
+                .build().awaitReady();
+
+
 
 
     }
